24592f81e7d50d313bce519677eea45e8e9eca47
JonaS94 <jonas94@MacBook-Pro-de-JonaS94.local>
2025-12-08 22:13:53 -0500

    fear:[PR-06][Jonathan] Editar CSS y implementar evento botones

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,4 +1,21 @@
+import ItemCount from "./ItemCount";
+import { useState } from "react";
+
 const ItemDetail = ({ item }) => {
+	const [cantidad, setCantidad] = useState(1);
+
+	const handleRestar = () => {
+		if (cantidad > 1) {
+			setCantidad(cantidad - 1);
+		}
+	};
+	const handleSumar = () => {
+		cantidad < item.stock && setCantidad(cantidad + 1);
+	};
+	const handleAgregar = () => {
+		console.log({ ...item, cantidad });
+	};
+
 	return (
 		<div className="container">
 			<div className="producto-detalle">
@@ -6,8 +23,14 @@ const ItemDetail = ({ item }) => {
 				<div>
 					<h3 className="titulo">{item.nombre}</h3>
 					<p className="descripcion">{item.descripcion}</p>
-					<p className="categoria">Categoria: {item.categoria.nombre}</p>
+					<p className="categoria">Categoria: {item.categoria?.nombre}</p>
 					<p className="precio">Precio: ${item.precio}</p>
+					<ItemCount
+						cantidad={cantidad}
+						handleSumar={handleSumar}
+						handleRestar={handleRestar}
+						handleAgregar={handleAgregar}
+					/>
 				</div>
 			</div>
 		</div>
